d29333a46dfefafdabb774f54ef3e31411cb679f
Tuliany <57564014+Tuliany@users.noreply.github.com>
2020-07-27 09:58:40 +0200

    Initial commit

---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+import React, { useState }from 'react'
+
+export const Button = ({submitMessage, handleClick, buttonStyle, buttonText}) => {
+  return (
+    <button type='submit' onClick={handleClick} className={buttonStyle}><p className={buttonText}>{submitMessage} </p> </button>
+  )
+}
---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Title = ({title, emoji,titleColor}) => {
+  return (
+    <header>
+      <h1 className={titleColor}>{title} 
+      </h1>
+    <div>
+    <span role='img'>{emoji}</span>
+    </div>
+    
+    </header>
+  )
+
+}
